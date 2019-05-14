0aca1a06f183aac24c2b9e27f9829c07b82d77d4
Emo <eiv.razgrad@gmail.com>
2019-05-14 16:58:16 +0300

    Simple modified => adding const string NewLine; test Thread Sleep; test status code 307 Moved Temporary

--- SoftUniHttpServer/StartUp.cs
+++ SoftUniHttpServer/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace SoftUniHttpServer
 {
@@ -9,29 +10,34 @@ namespace SoftUniHttpServer
     {
         private const int Port = 80;
         private const int Buffer = 10000;
+        private const string NewLine = "\r\n";
 
         public static void Main(string[] args)
         {
             var tcpListener = new TcpListener(IPAddress.Loopback, Port);
             tcpListener.Start();
             //
+            StringBuilder stringBuilder = new StringBuilder();
 
             while (true)
             {
                 var client = tcpListener.AcceptTcpClient();
-
+                //Todo Mozila
                 using (NetworkStream stream = client.GetStream())
                 {
                     var requestBytes = new byte[Buffer];
+                    Thread.Sleep(10000);
                     var readBytes = stream.Read(requestBytes, 0, requestBytes.Length);
-                    var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
+                    var stringRequest = Encoding.ASCII.GetString(requestBytes, 0, readBytes);
                     Console.WriteLine(new string('=', 70));
                     Console.WriteLine(stringRequest);
-                    var responseBody = "<form method='post'>input type='text' name='tweet' placeholder='Enter  tweet ...' /><input name='name' /><input type='submit' /></form>";
-                    var response = "HTTP/1.0 200 OK" + Environment.NewLine
-                        + "Content-Type: text/html" + Environment.NewLine +
-                        "Server: MyCustomServer/1.0" + Environment.NewLine +
-                        $"Content-Length: {responseBody.Length}" + Environment.NewLine + Environment.NewLine +
+                    var responseBody = "<form method='post'><input type='text' name='tweet' placeholder='Enter  tweet ...' /><input name='name' /><input type='submit' /></form>";
+                    var response = "HTTP/1.0 307 Moved Temporary" + NewLine
+                        + "Content-Type: text/html" + NewLine
+                        + "Location: https://google.com"+ NewLine
+
+                        + "Server: MyCustomServer/1.0" + NewLine +
+                        $"Content-Length: {responseBody.Length}" + NewLine + NewLine +
                         responseBody;
                     var responseBytes = Encoding.UTF8.GetBytes(response);
                     stream.Write(responseBytes, 0, responseBytes.Length);
